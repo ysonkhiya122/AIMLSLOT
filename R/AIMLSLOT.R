88c93d83cd694fa191a8eb8bedb79feea8734ac8
ysonkhiya122 <108086425+ysonkhiya122@users.noreply.github.com>
2023-02-16 11:56:51 +0530

    Update AIMLSLOT.R

--- R/AIMLSLOT.R
+++ R/AIMLSLOT.R
@@ -12,5 +12,7 @@ AIML=function(x){
   #numeric summary
   data.frame(min=min(x),
              median=median(x),
-             max=max(x))
+             max=max(x)),
+             sd=sd(x)),
+             range=max(x)-min(x)
 }
